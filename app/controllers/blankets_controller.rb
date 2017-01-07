807320c2d53677fb0fb8fe3f4d8c968a2c91152d
Seth Charles <swcharl@gmail.com>
2017-01-06 20:50:31 -0600

    Create Blankets controller and basic views

---
+++ app/controllers/blankets_controller.rb
@@ -0,0 +1,25 @@
+class BlanketsController < ApplicationController
+  def index
+  end
+
+  def new
+    @blanket = Blanket.new
+  end
+
+  def create
+    @blanket = Blanket.new(blanket_params)
+    @blanket.save
+
+    redirect_to @blanket
+  end
+
+  def show
+  end
+
+  private
+
+  def blanket_params
+    params.require(:blanket).permit(:name, :start_date, :end_date, :latitude, :longitude)
+  end
+
+end
