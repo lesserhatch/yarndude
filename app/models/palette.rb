967a751c72933c9b655b239f9accd62c8929dde4
Seth Charles <swcharl@gmail.com>
2017-01-13 12:57:20 -0600

    Add protection to palette find yarn function

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -3,9 +3,11 @@ class Palette < ApplicationRecord
   has_many :yarns, through: :temperature_ranges
 
   def find_yarn_by_temperature(temperature)
+    return nil if temperature.nil?
     self.temperature_ranges.each do |temperature_range|
       return temperature_range.yarn if ( temperature_range.low_temperature <= temperature &&  temperature <= temperature_range.high_temperature)
     end
+    nil
   end
 
 end
