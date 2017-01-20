82fe6863e9cfa00385cc92127ea7fed97a4a7171
Seth Charles <swcharl@gmail.com>
2017-01-19 21:12:05 -0600

    Switch to dotenv on production to set AWS creds

--- Gemfile
+++ Gemfile
@@ -61,10 +61,12 @@ gem 'stripe'
 gem 'kaminari'
 gem 'bootstrap4-kaminari-views'
 
+# Secrets
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
-  gem 'dotenv-rails'
 end
 
 group :development do
--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :repo_url, "git@gitlab.com:swcharl/yarndude.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/.well-known"
