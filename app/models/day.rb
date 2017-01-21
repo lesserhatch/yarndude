15c0d77e22f7404500d6a5a4fbfcc5de38d5182b
Seth Charles <swcharl@gmail.com>
2017-01-21 13:30:43 -0600

    Add validations to Day model

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,15 @@
 class Day < ApplicationRecord
+  # Associations
   belongs_to :blanket
+
+  # Standard validations
+  validates :blanket, presence: true
+  validates :low_temperature, numericality: { only_integer: true }
+  validates :high_temperature, numericality: { only_integer: true }
+  validates :date, presence: true
+
+  # Custom validations
+  validate :low_cannot_be_greater_than_high
 
   def temperature_in(degrees, units)
     return degrees if units == :farhenheit
@@ -12,5 +22,15 @@ class Day < ApplicationRecord
 
   def high_temperature_in(units)
     temperature_in(self.high_temperature, units)
+  end
+
+  private
+
+  def low_cannot_be_greater_than_high
+    unless self.low_temperature.blank? || self.high_temperature.blank?
+      if self.low_temperature > self.high_temperature
+        errors.add(:low_temperature, "can't be greater than high_temperature")
+      end
+    end
   end
 end
